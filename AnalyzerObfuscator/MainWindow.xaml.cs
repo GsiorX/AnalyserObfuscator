f3ea2da245389da398c46a99e618d47aedb69916
Jakub Gasiorek <220890@student.pwr.edu.pl>
2020-10-15 12:07:37 +0200

    File picker

--- AnalyzerObfuscator/MainWindow.xaml.cs
+++ AnalyzerObfuscator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,36 @@ namespace AnalyzerObfuscator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if(result == true)
+            {
+                string fileName = openFileDialog.FileName;
+
+                //Info dla użytkownika
+                file1.Text = fileName.Split("\\").Last();
+            }
+        }
+
+        private void btnOpenFile2_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            Nullable<bool> result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string fileName = openFileDialog.FileName;
+
+                //Info dla użytkownika
+                file2.Text = fileName.Split("\\").Last();
+            }
         }
     }
 }
