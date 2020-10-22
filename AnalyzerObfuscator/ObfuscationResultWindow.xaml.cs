dda9d6edecb0adc6f9682f1c41d869daa956d2de
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-10-22 13:01:29 +0200

    Add window with obfuscation results

---
+++ AnalyzerObfuscator/ObfuscationResultWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AnalyzerObfuscator
+{
+    /// <summary>
+    /// Interaction logic for ObfuscationResultWindow.xaml
+    /// </summary>
+    public partial class ObfuscationResultWindow : Window
+    {
+        public ObfuscationResultWindow(FlowDocument document)
+        {
+            InitializeComponent();
+            Analyze(document);
+        }
+
+        void Analyze(FlowDocument document)
+        {
+            string text = new TextRange(document.ContentStart, document.ContentEnd).Text;
+            GeneralizationObfuscator genObfs = new GeneralizationObfuscator();
+            string genObfsRes = genObfs.ObfuscateText(text);
+            textBlock.Text = genObfsRes;
+        }
+    }
+}
