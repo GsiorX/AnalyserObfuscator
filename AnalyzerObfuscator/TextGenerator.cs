4361de698f8ffc767c77e430306887dfb2b0536a
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-11-11 18:54:38 +0100

    Create vocabulary dictionary and text generator

---
+++ AnalyzerObfuscator/TextGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AnalyzerObfuscator
+{
+    class TextGenerator
+    {
+        static string generateSentence(Random rand)
+        {
+            int nounId = rand.Next(0, Vocabulary.nouns.Count);
+            int advId = rand.Next(0, Vocabulary.adjectives.Count);
+            int verbId = rand.Next(0, Vocabulary.verbs.Length);
+
+            var nounParticlePair = Enumerable.ToList(Vocabulary.nouns)[nounId];
+            var advParticlePair = Enumerable.ToList(Vocabulary.adjectives)[advId];
+            // var nounParticle = nounParticlePair.Value;
+            var noun = nounParticlePair.Key;
+            var adv = advParticlePair.Key;
+            var advParticle = advParticlePair.Value;
+            var verb = Vocabulary.verbs[verbId];
+
+            return String.Format("{0} {1} {2} is {3}ing.", advParticle, adv, noun, verb);
+        }
+
+        public static string generateText(int length)
+        {
+            if (length < 1)
+            {
+                return String.Empty;
+            }
+
+            var rand = new Random();
+            string res = generateSentence(rand);
+            for (int i = 0; i < length; i++)
+            {
+                res += " " + generateSentence(rand);
+            }
+            return res;
+        }
+    }
+}
