2b27b25bfbc20adfcc5831b53f7c05f65a6064a7
AlexGerasi <ola.gerasimuk@gmail.com>
2020-11-16 18:08:23 +0100

    Add avg words per sentence and letters per word

--- AnalyzerObfuscator/BasicAnalizer.cs
+++ AnalyzerObfuscator/BasicAnalizer.cs
@@ -9,9 +9,12 @@ namespace AnalyzerObfuscator
     {
         public List<(string, double)> AnalyzeSingleText(string text, string name)
         {
-            double wordCount = 0, sentenceCount = 0, lettersCount = 0;
-            const char dot = '.';
-            int index = 0;
+            double wordCount = 0, sentenceCount = 0, lettersCount = 0, wordPerSentResult = 0, lettersPerWordResult = 0; ;
+            const char dot = '.', exclamation = '!', question = '?';
+
+            int index = 0, wordPerSentIndex = 0, lettersPerWordIndex = 0; ;
+            List<int> wordsPerSentence = new List<int>();
+            List<int> lettersPerWord = new List<int>();
 
             // skip whitespace until first word
             while (index < text.Length && char.IsWhiteSpace(text[index]))
@@ -24,19 +27,44 @@ namespace AnalyzerObfuscator
                 {
                     index++;
                     lettersCount++;
+                    lettersPerWordIndex++;
                 }
 
                 wordCount++;
+                lettersPerWord.Add(lettersPerWordIndex);
+                lettersPerWordIndex = 0;
+                wordPerSentIndex++;
 
                 if (text[index] == dot)
+                {
                     sentenceCount++;
+                    wordsPerSentence.Add(wordPerSentIndex);
+                    wordPerSentIndex = 0;
+                }
 
                 // skip whitespace until next word
                 while (index < text.Length && (char.IsWhiteSpace(text[index]) || text[index] == dot))
                     index++;
             }
+            foreach (int count in wordsPerSentence)
+            {
+                wordPerSentResult += count;
+            }
 
-            return new List<(string, double)>() { ("Number of letters in " + name, lettersCount), ("Number of words in " + name, wordCount), ("Number of sentences in " + name, sentenceCount) };
+            foreach (int count in lettersPerWord)
+            {
+                lettersPerWordResult += count;
+            }
+
+            wordPerSentResult /= sentenceCount;
+            lettersPerWordResult /= wordCount;
+
+            return new List<(string, double)>() {
+                ("Number of letters in " + name, lettersCount),
+                ("Number of words in " + name, wordCount),
+                ("Number of sentences in " + name, sentenceCount),
+                ("Average number of words in a sentence in " + name, Math.Round(wordPerSentResult, 2)),
+                ("Average number of letters in a word in " + name, Math.Round(lettersPerWordResult, 2))};
         }
         public List<(string, double)> AnalyzeText(string text, string obfuscated)
         {
