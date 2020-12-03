00f78ca28d4fa63678f86008dbdc232521ea3057
Jakub Gasiorek <220890@student.pwr.edu.pl>
2020-12-03 12:06:28 +0100

    Add tabs to main window ui, add xaml document generator to main program

--- AnalyzerObfuscator/MainWindow.xaml.cs
+++ AnalyzerObfuscator/MainWindow.xaml.cs
@@ -25,75 +25,9 @@ namespace AnalyzerObfuscator
     /// </summary>
     public partial class MainWindow : Window
     {
-        string documentName;
-        string obfDocumentName;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            documentName = System.IO.Path.Combine(Environment.CurrentDirectory, @"..\\..\\..\\test documents\test1a.xaml");
-            file1.Text = "test1a.xaml";
-            obfDocumentName = System.IO.Path.Combine(Environment.CurrentDirectory, @"..\\..\\..\\test documents\test1b.xaml");
-            file2.Text = "test1b.xaml";
-        }
-
-        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            Nullable<bool> result = openFileDialog.ShowDialog();
-
-            if(result == true)
-            {
-                documentName = openFileDialog.FileName;
-
-                //Info dla użytkownika
-                file1.Text = documentName.Split("\\").Last();
-            }
-        }
-
-        private void btnOpenFile2_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-
-            Nullable<bool> result = openFileDialog.ShowDialog();
-
-            if (result == true)
-            {
-                obfDocumentName = openFileDialog.FileName;
-
-                //Info dla użytkownika
-                file2.Text = obfDocumentName.Split("\\").Last();
-            }
-        }
-
-        private void next_Click(object sender, RoutedEventArgs e)
-        {
-            if (analyseBtn.IsChecked == true)
-            { 
-                ResultWindow resultWindow = new ResultWindow(documentName, obfDocumentName);
-                resultWindow.Show();
-            }
-
-            if (obfuscateBtn.IsChecked == true)
-            {
-                FlowDocument document = Helpers.SetRTF(documentName);
-
-                ObfuscationResultWindow resultWindow = new ObfuscationResultWindow(document);
-                resultWindow.Show();
-            }
-
-        }
-
-        private void obfuscateBtn_Checked(object sender, RoutedEventArgs e)
-        {
-            btnOpenFile2.IsEnabled = false;
-        }
-
-        private void analyseBtn_Checked(object sender, RoutedEventArgs e)
-        {
-            btnOpenFile2.IsEnabled = true;
         }
     }
 }
