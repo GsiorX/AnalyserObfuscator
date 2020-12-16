9c1ac09dc165a5bb2d26701dc857a829c05d5b1d
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-12-16 20:44:50 +0100

    Enhance generator + minor changes

--- AnalyzerObfuscator/Obfuscators/AndObfuscator.cs
+++ AnalyzerObfuscator/Obfuscators/AndObfuscator.cs
@@ -11,13 +11,16 @@ namespace AnalyzerObfuscator
             string[] sentences = text.Split(".");
             List<string> outputSentences = new List<string>();
 
-            outputSentences.Add(sentences[0]);
+            // outputSentences.Add(sentences[0]);
 
             for (int i = 1; i < sentences.Length - 1; i++)
             {
                 outputSentences.Add(Vocabulary.lower(sentences[i]));
             }
-            return string.Join(" and ", outputSentences);
+            string tail = string.Join(" and ", outputSentences);
+
+            if (tail == "") return sentences[0];
+            else return sentences[0] + " and " + string.Join(", and ", outputSentences);
         }
     }
 }
--- AnalyzerObfuscator/TextGenerator.cs
+++ AnalyzerObfuscator/TextGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AnalyzerObfuscator
@@ -12,18 +13,20 @@ namespace AnalyzerObfuscator
 
             return char.ToUpper(word[0]) + word.Substring(1);
         }
-        static string generateSentence(Random rand)
+        static string generateSentence(Random rand, Noun psub = null)
         {
             int nounId = rand.Next(0, Vocabulary.subjects.Count);
-            int advId = rand.Next(0, Vocabulary.adjectives.Count);
+            // int advId = rand.Next(0, Vocabulary.adjectives.Count);
             int verbId = rand.Next(0, Vocabulary.verbs.Count);
 
             var subParticlePair = Enumerable.ToList(Vocabulary.subjects)[nounId];
-            var advParticlePair = Enumerable.ToList(Vocabulary.adjectives)[advId];
+            // var advParticlePair = Enumerable.ToList(Vocabulary.adjectives)[advId];
             // var nounParticle = nounParticlePair.Value;
-            var subject = subParticlePair.Key;
-            var adv = advParticlePair.Key;
-            var advParticle = advParticlePair.Value;
+            var nsub = (psub == null) ? subParticlePair.Value : psub;
+            var subject = (psub == null) ? subParticlePair.Key : psub.Text;
+            var subjectParticle = (psub == null) ? subParticlePair.Value.Particle : psub.Particle;
+            // var adv = advParticlePair.Key;
+            // var advParticle = advParticlePair.Value;
             var verb = Vocabulary.verbs[verbId];
 
             if (verb.Tags.Count > 0)
@@ -32,12 +35,84 @@ namespace AnalyzerObfuscator
                 var n = Noun.GetByTag(Vocabulary.nouns, tag);
                 if (n.Count > 0)
                 {
+                    int advId = rand.Next(0, Vocabulary.adjectives.Count);
+                    var advParticlePair = Enumerable.ToList(Vocabulary.adjectives)[advId];
+                    var adv = advParticlePair.Key;
+                    var advParticle = advParticlePair.Value;
+
                     var cn = n[(rand.Next(0, n.Count))];
-                    return String.Format("{0} {1} {2} is {3} {4} {5}.", capitalize(advParticle), adv, subject, verb.Continuous, cn.Particle, cn.Text);
+                    return String.Format("{0} {1} {2} was {3} {4} {5}.", capitalize(advParticle), adv, subject, verb.Continuous, cn.Particle, cn.Text);
                 }
             }
 
-            return String.Format("{0} {1} {2} is {3}.", capitalize(advParticle), adv, subject, verb.Continuous);
+            bool isContinuous = rand.NextDouble() < 0.2;
+            int numberOfAdj = psub == null ? (rand.NextDouble() < 0.6 ? (rand.NextDouble() < 0.2 ? 2 : 1) : 0) : 0;
+            bool useParticleThe = rand.NextDouble() < 0.3;
+
+            List<string> genWords = new List<string>();
+
+            List<string> adjList = new List<string>();
+            string particle = subjectParticle;
+            for (int i = 0; i < numberOfAdj; i++)
+            {
+                int advId = rand.Next(0, Vocabulary.adjectives.Count);
+                var advParticlePair = Enumerable.ToList(Vocabulary.adjectives)[advId];
+
+                particle = advParticlePair.Value;
+
+                adjList.Add(advParticlePair.Key);
+            }
+            adjList.Reverse();
+
+            if (psub != null)
+            {
+                genWords.AddRange(new List<string>() 
+                {
+                    "then"
+                });
+            }
+
+            if (psub == null )
+            {
+                if (rand.NextDouble() < 0.3)
+                {
+                    genWords.Add("some");
+                }
+                else
+                {
+                    genWords.Add(particle);
+                }
+            }
+            else if (useParticleThe)
+            {
+                genWords.Add("the");
+            }
+            else
+            {
+                genWords.Add("that");
+            }
+            
+            genWords.AddRange(adjList);
+            genWords.Add(subject);
+            if (isContinuous)
+            {
+                genWords.Add("was");
+                genWords.Add(verb.Continuous);
+            } 
+            else
+            {
+                genWords.Add(verb.Past);
+            }
+
+            genWords[0] = capitalize(genWords[0]);
+
+            string nextSentence = rand.NextDouble() < 0.8 ? " " + generateSentence(rand, nsub) : "";
+            return string.Join(" ", genWords) + "." + nextSentence;
+            //if (isContinuous)
+            //    return String.Format("{0} {1} {2} was {3}.", capitalize(particle), adjList, subject, verb.Continuous);
+            //else
+            //    return String.Format("{0} {1}{2} {3}.", capitalize(particle), adjList, subject, verb.Past);
+            
         }
 
         public static string generateText(int length)
@@ -48,10 +123,15 @@ namespace AnalyzerObfuscator
             }
 
             var rand = new Random();
+
             string res = generateSentence(rand);
-            for (int i = 0; i < length; i++)
+            int sentencesCount = res.Split(".").Length - 1;
+
+            for (int i = 0; sentencesCount < length; i++)
             {
-                res += " " + generateSentence(rand);
+                string nextSentences = generateSentence(rand);
+                res += " " + nextSentences;
+                sentencesCount += nextSentences.Split(".").Length - 1;
             }
             return res;
         }
