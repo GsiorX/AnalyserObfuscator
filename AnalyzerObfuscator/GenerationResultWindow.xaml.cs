a7d7f2ebe108c662a0105d7bb319166184b384e0
Jakub Gasiorek <220890@student.pwr.edu.pl>
2020-12-16 13:32:10 +0100

    Show generated document before saving

---
+++ AnalyzerObfuscator/GenerationResultWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AnalyzerObfuscator
+{
+    /// <summary>
+    /// Logika interakcji dla klasy GenerationResultWindow.xaml
+    /// </summary>
+    public partial class GenerationResultWindow : Window
+    {
+        string _documentText;
+        public GenerationResultWindow(string documentText)
+        {
+            InitializeComponent();
+            _documentText = documentText;
+            Display();
+        }
+
+        void Display()
+        {
+            FlowDocument content = XamlReader.Parse(_documentText) as FlowDocument;
+            reader.Document = content;
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ValidateNames = false;
+            saveFileDialog.CheckFileExists = false;
+            saveFileDialog.FileName = "generated file.xaml";
+            saveFileDialog.Filter = "Xaml file (*.xaml)|*.xaml";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    File.WriteAllText(saveFileDialog.FileName, _documentText);
+
+                    MessageBox.Show("File saved", "OK", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception _)
+                {
+                    MessageBox.Show("Failed to save a file", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+    }
+}
