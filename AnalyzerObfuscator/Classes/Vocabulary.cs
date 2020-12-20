04507014ce627c73f7bb5e16e5511f13d5a66ca6
alexgerasimuk <235010@student.pwr.edu.pl>
2020-12-20 17:20:02 +0100

    Irregular plural nouns

--- AnalyzerObfuscator/Classes/Vocabulary.cs
+++ AnalyzerObfuscator/Classes/Vocabulary.cs
@@ -28,15 +28,15 @@ namespace AnalyzerObfuscator
             { "animal", new Noun("an", "animal", new List<string>(){ "animal"}, "s") },
             { "dog", new Noun("a", "dog", new List<string>(){ "animal" },"s")},
             { "cat", new Noun("a", "cat", new List<string>(){ "animal" }, "s" )},
-            { "fish", new Noun("a", "fish", new List<string>(){ "animal" }, "es" )},
-            { "mouse", new Noun("a", "mouse", new List<string>(){ "animal"}, "s" ) },
+            { "fish", new Noun("a", "fish", new List<string>(){ "animal" }, "" )},
+            { "mouse", new Noun("a", "mouse", new List<string>(){ "animal"}, "mice" ) },
             { "lion", new Noun("a", "lion", new List<string>(){ "animal" }, "s") },
-            { "person", new Noun("a", "person", new List<string>(){ "person" }, "s") },
-            { "man", new Noun("a", "man", new List<string>(){ "person" } , "s")},
-            { "woman", new Noun("a", "woman", new List<string>(){ "person" }, "s") },
+            { "person", new Noun("a", "person", new List<string>(){ "person" }, "people") },
+            { "man", new Noun("a", "man", new List<string>(){ "person" } , " men")},
+            { "woman", new Noun("a", "woman", new List<string>(){ "person" }, "women") },
             { "boy", new Noun("a", "boy", new List<string>(){ "person" }, "s") },
             { "girl", new Noun("a", "girl", new List<string>(){ "person" }, "s") },
-            { "child", new Noun("a", "child", new List<string>(){ "person" }, "s") },
+            { "child", new Noun("a", "child", new List<string>(){ "person" }, "children") },
             { "father", new Noun("a", "father", new List<string>(){ "person" }, "s") },
             { "mother", new Noun("a", "mother", new List<string>(){ "person" }, "s") },
             { "hat", new Noun("a", "hat", new List<string>(){ "object" }, "s") },
@@ -45,10 +45,10 @@ namespace AnalyzerObfuscator
             { "surgeon", new Noun("a", "surgeon", new List<string>(){ "person" }, "s") },
             { "monster", new Noun("a", "monster", new List<string>(){ "person" }, "s") },
             { "rat", new Noun("a", "rat", new List<string>(){ "animal" }, "s") },
-            { "wolf", new Noun("a", "wolf", new List<string>(){ "animal" }, "s") },
-            { "country", new Noun("a", "country", new List<string>(){ "place" }, "es") },
-            { "village", new Noun("a", "village", new List<string>(){ "place" }, "es") },
-            { "city", new Noun("a", "city", new List<string>(){ "city" }, "s") }
+            { "wolf", new Noun("a", "wolf", new List<string>(){ "animal" }, "wolves") },
+            { "country", new Noun("a", "country", new List<string>(){ "place" }, "countries") },
+            { "village", new Noun("a", "village", new List<string>(){ "place" }, "s") },
+            { "city", new Noun("a", "city", new List<string>(){ "city" }, "cities") }
         };
 
         public static readonly Dictionary<string, Noun> nouns = new Dictionary<string, Noun>() {
